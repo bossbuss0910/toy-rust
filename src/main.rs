a05cb009915bc29940595fcae62e3855408aee8a
bossbuss0910 <bumpof8265chicken@yahoo.co.jp>
2018-02-18 16:21:28 +0900

    偶数2乗和だって

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// rustはスネークケース
+fn square_sum(n: isize) -> isize { //　いまだわからないisizeとか
+    (0..n).  // この書き方出来るのは美しい rubyみたい
+        filter(|i| i % 2 == 0) // なんでこういう書き方になったのかわからん。。。
+        .map(|i| i * i) // scala でいう .map(i => i * i) みたいな感じ
+        .sum() //return いらない
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", square_sum(20))
 }
