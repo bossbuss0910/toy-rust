806e31d53af0c3553a2dc24d792c45e1e197c572
bossbuss0910 <bumpof8265chicken@yahoo.co.jp>
2018-02-18 16:37:48 +0900

    変数束縛！

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 2 + 3;
+    // x = 8; を書くとエラーになる! immutable!
+    println!("{}", x);
+    let mut y = 2 + 3; // 警告出るけどmutableに一応出来る。多分あんまりやらない
+    y = 8;
+    println!("{}", y);
+
+    let mut sokubaku = x; //mutableの変数に束縛出来る
+    sokubaku += 2; //ちゃっかり += も出来るんだ！
+    println!("{}", sokubaku);
+
 }
